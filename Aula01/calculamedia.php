fd83688a7cc314137d744c22926900cf22c64e1d
Ana Beatriz Ribeiro Soares <159508471+Anabti@users.noreply.github.com>
2026-02-19 10:49:02 -0300

    Add files via upload

---
+++ Aula01/calculamedia.php
@@ -0,0 +1,13 @@
+<?php
+    $n1 = 8;
+    $n2 = 7;
+    $n3 = 9;
+
+    $media = ($n1 + $n2 + $n3) / 3;
+
+    echo "Nota 1:8 <br>";
+    echo "Nota 2:7 <br>";
+    echo "Nota 3:9 <br>";
+    echo " A média é :". $media;
+
+?>
