7bb46b6bd019aefa0aeb9691709123b5883d77c1
Ana Beatriz Ribeiro Soares <159508471+Anabti@users.noreply.github.com>
2026-04-09 08:44:55 -0300

    Add files via upload

---
+++ Aula02/exercicio01.php
@@ -0,0 +1,12 @@
+<?php
+    echo "EXERCÍCIO 01:";
+    echo '<hr>';
+
+    $nome = 'Ana Beatriz';
+    $idade = 18;
+    $cidade = 'Ribeirão Pires';
+
+    echo "Seu nome é " . $nome . ", você tem " . $idade . " anos e mora em " . $cidade . ".";
+    echo '<br>';
+
+?>
