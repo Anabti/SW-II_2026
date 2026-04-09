200506dd53ff7652f9e4f240a24a88db57e46957
Ana Beatriz Ribeiro Soares <159508471+Anabti@users.noreply.github.com>
2026-04-09 08:34:03 -0300

    Update exercicio01.php

--- Aula02/exercicio01.php
+++ Aula02/exercicio01.php
@@ -2,8 +2,8 @@
     echo "EXERCÍCIO 01:";
     echo '<hr>';
 
-    $nome = 'Geovana Brito';
-    $idade = 17;
+    $nome = 'Ana Beatriz Ribeiro Soares';
+    $idade = 18;
     $cidade = 'Ribeirão Pires';
 
     echo "Seu nome é " . $nome . ", você tem " . $idade . " anos e mora em " . $cidade . ".";
